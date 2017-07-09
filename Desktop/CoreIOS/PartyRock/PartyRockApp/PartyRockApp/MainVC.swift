d2f9d56aac6fb6570e4bb1a0e96d4f65afe005a5
Blakey47 <darragh.blake48@gmail.com>
2017-07-09 20:18:02 +0100

    First Commit

---
+++ Desktop/CoreIOS/PartyRock/PartyRockApp/PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Darragh on 7/8/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
